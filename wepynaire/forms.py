c58b2f5a6458ce94b27fc2ba053db9a1ff8d8f88
tchappui <tchappui@gmail.com>
2023-01-30 18:08:44 +0100

    Ajout des formhelper

--- wepynaire/forms.py
+++ wepynaire/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth import forms as auth_forms
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Div, Row, Column
+
 from .models import UserAddress, Address
 
 User = get_user_model()
@@ -14,11 +17,23 @@ class UserCreationForm(auth_forms.UserCreationForm):
         model = User
         fields = ("name", "username")
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.help_text_inline = False
+        self.helper.layout = Layout(
+            "name",
+            "username",
+            "password1",
+            "password2",
+        )
+
 
 class AddressCreationForm(forms.ModelForm):
     """Formulaire de création pour une adresse."""
 
-    address_type = forms.ChoiceField(label="type de l'adresse")
+    address_type = forms.ChoiceField(label="Type de l'adresse")
 
     class Meta:
         model = Address
@@ -34,3 +49,14 @@ class AddressCreationForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields["address_type"].choices = UserAddress.AdresseRoles.choices
+
+        self.helper = FormHelper()
+        self.helper.form_tag = False
+        self.helper.layout = Layout(
+            "street",
+            "street_number",
+            "address_complement",
+            "city",
+            "zip_code",
+            "country",
+        )

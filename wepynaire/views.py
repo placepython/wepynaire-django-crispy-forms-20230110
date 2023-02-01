62fd01440a69146fa2decb6850ff2777e491abd0
tchappui <tchappui@gmail.com>
2023-02-01 16:50:00 +0100

    Fin de la version simple

--- wepynaire/views.py
+++ wepynaire/views.py
@@ -5,72 +5,45 @@ from django.views.generic import View
 from .forms import UserCreationForm, AddressCreationForm
 from .models import UserAddress
 
-# Création d'une CBV simpliste pour gérer plusieurs formulaires
-class MultiFormsView(View):
-    """Vue générique simple gérant plusieurs formulaires."""
 
-    template_name = None
-    form_classes = None
-    success_url = None
+class HomeView(View):
+    """Vue gérant l'affichage de la page d'accueil."""
 
-    def get_forms(self):
-        return [form(self.request.POST or None) for form in self.form_classes]
-
-    def render_to_response(self, **context):
-        """Retourne le template avec le contexte."""
-        return render(
-            self.request,
-            self.template_name,
-            context,
-        )
-
-    def all_valid(self):
-        """Retourne True si tous les formulaires sont valides."""
-        if all(form.is_valid() for form in self.forms):
-            return True
-        return False
-
-    def form_valid(self):
-        """Actions à entreprendre si tous les formulaires sont valides."""
-        return redirect(str(self.success_url))
-
-    def form_invalid(self):
-        """Actions à entreprendre si un formulaire est invalide."""
-        return self.render_to_response(forms=self.forms)
-
-    def get(self, request, *args, **kwargs):
-        """Réponds à l'affichage du formulaire."""
-        self.forms = self.get_forms()
-        return self.render_to_response(forms=self.forms)
-
-    def post(self, request, *args, **kwargs):
-        """Réponds à la soumission du formulaire."""
-        self.forms = self.get_forms()
-        if self.all_valid():
-            return self.form_valid()
-        else:
-            return self.form_invalid()
-
-
-# Création de notre HomeView à l'aide de la CBV réutilisable
-class HomeView(MultiFormsView):
-    """Vue gérant la page d'accueil du site."""
-
-    template_name = "wepynaire/home.html"
     form_classes = [UserCreationForm, AddressCreationForm]
+    template_name = "wepynaire/home.html"
     success_url = "wepynaire:home"
 
-    def form_valid(self):
-        user_form, address_form = self.forms
-        user = user_form.save()
-        address = address_form.save()
-        address_type = address_form.cleaned_data.get("address_type")
-        UserAddress.objects.create(
-            user=user,
-            address=address,
-            address_role=address_type,
+    def get(self, request):
+        """Réponds aux requêtes GET."""
+        forms = [form() for form in self.form_classes]
+        return render(
+            request,
+            self.template_name,
+            {"forms": forms},
         )
-        return super().form_valid()
+
+    def post(self, request):
+        """Réponds aux requêtes POST."""
+        forms = [form(request.POST) for form in self.form_classes]
+        if all(form.is_valid() for form in forms):
+            # Tous les formulaires sont valides
+            user_form, address_form = forms
+            user = user_form.save()
+            address = address_form.save()
+            address_type = address_form.cleaned_data.get("address_type")
+            UserAddress.objects.create(
+                user=user,
+                address=address,
+                address_role=address_type,
+            )
+            return redirect(self.success_url)
+        else:
+            # au moins un formulaire est invalide
+            return render(
+                request,
+                self.template_name,
+                {"forms": forms},
+            )
 
 
 home_view = HomeView.as_view()
